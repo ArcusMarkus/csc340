d70dd7d1f2e00fe5383bd9e7a5efeaca34e3e222
ArcusMarkus <belmonteMM@hendrix.edu>
2026-05-11 00:52:40 -0500

    anime api implemented/typos fixed

--- csci340lab4/script.js
+++ csci340lab4/script.js
@@ -1,8 +1,24 @@
-let navExpanded = true
+document.getElementById("animebtn").addEventListener("click", () =>{
 
-function toggleNav()
-{
+    const anime = document.getElementById("animeInput").value;
 
-    
-    navExpanded = !navExpanded
-}
+    fetch (`https://api.jikan.moe/v4/anime?q=${anime}&limit=1`)
+    .then(response => response.json())
+    .then(data => {
+
+        const animeInfo = data.data[0];
+
+        document.getElementById("animeData").innerHTML = `
+            <h3>${animeInfo.title}</h3>
+            
+            <img src="${animeInfo.images.jpg.image_url}" width="250">
+
+            <p><strong>Score:</strong> ${animeInfo.score}</p>
+
+            <p><strong>Episodes:</strong> ${animeInfo.episodes}</p>
+
+            <p>${animeInfo.synopsis.substring(0, 300)}...</p>
+
+            `;
+    });
+});
